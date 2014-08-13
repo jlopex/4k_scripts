04105c808ee74d8cb2625d92a9886689d59caa7a
Javier Lopez <javier.lopez@wuaki.tv>
2014-08-13 09:43:46 +0200

    First working version

--- yuv_16_9.sh
+++ yuv_16_9.sh
@@ -9,16 +9,19 @@ fi
 OUTPUT_WIDTH=3840
 OUTPUT_HEIGHT=2160
 
-# Very ugly but it works!
+# Very ugly but it gets the current FPS!
 FRAME_RATE=$(ffmpeg -i ${1} 2>&1 | grep 'fps' | sed -n 's/.*\([0-9][0-9]\) fps.*/\1/p')
 
-# We create a fifo to duplicate the pipe
-mkfifo pipe
+# This command will create a 3840x2160 yuv raw (A) at original frame rate, appending padding if necessary
+ffmpeg -i $1 -f rawvideo -pix_fmt yuv420p -vf "scale=iw*sar:ih , pad=max(iw\,ih*(16/9)):ow/(16/9):(ow-iw)/2:(oh-ih)/2, scale=${OUTPUT_WIDTH}x${OUTPUT_HEIGHT}" - | NGS_FragmentPreprocessorYUV -w ${OUTPUT_WIDTH} -h ${OUTPUT_HEIGHT} -f ${FRAME_RATE} --A > A_${2}.yuv
 
-# We launch the ffmpeg with rescaling to 3840x2160 adding padding if necessary
-ffmpeg -i ${1} -vf "scale=iw*sar:ih , pad=max(iw\,ih*(16/9)):ow/(16/9):(ow-iw)/2:(oh-ih)/2, scale=${OUTPUT_WIDTH}x${OUTPUT_HEIGHT}" \
-       -f rawvideo -pix_fmt yuv420p - | tee pipe | NGS_FragmentPreprocessorYUV -w ${OUTPUT_WIDTH} -h ${OUTPUT_HEIGHT}-f 30 --APrime > AP_h265.yuv
+# This command will create a 3840x2160 yuv raw (AP) at original frame rate, appending padding if necessary (THIS MIGHT ME BE GENERATED LATER)
+ffmpeg -i $1 -f rawvideo -pix_fmt yuv420p -vf "scale=iw*sar:ih , pad=max(iw\,ih*(16/9)):ow/(16/9):(ow-iw)/2:(oh-ih)/2, scale=${OUTPUT_WIDTH}x${OUTPUT_HEIGHT}" - | NGS_FragmentPreprocessorYUV -w ${OUTPUT_WIDTH} -h ${OUTPUT_HEIGHT} -f ${FRAME_RATE} --APrime > AP_${2}.yuv
 
-# Read the pipe to generate the second YUV
-cat pipe | (NGS_FragmentPreprocessorYUV -w ${OUTPUT_WIDTH} -h ${OUTPUT_HEIGHT} -f ${FRAME_RATE} --A > A_h265.yuv) &
+ffmpeg -y -f rawvideo -s ${OUTPUT_WIDTH}x${OUTPUT_HEIGHT} -r ${FRAME_RATE} -i A_${2}.yuv -preset fast -x265-params level=5.1:bitrate=8000:vbv-bufsize=24000:bframes=3:ref=4:keyint=48:min-keyint=24:scenecut=0:b-adapt=1:b-pyramid=0:tskip=1 -movflags +faststart -map_chapters -1 -f mp4 -vcodec libx265 -b:v 8000k -minrate 8000k -maxrate 8000k -bufsize 24000k -s:v:0 ${OUTPUT_WIDTH}x${OUTPUT_HEIGHT} -coder 1 -an -pass 1 /dev/null
+
+ffmpeg -y -f rawvideo -s ${OUTPUT_WIDTH}x${OUTPUT_HEIGHT} -r ${FRAME_RATE} -i A_${2}.yuv -i ${1} -preset fast -x265-params level=5.1:bitrate=8000:vbv-bufsize=24000:bframes=3:ref=4:keyint=48:min-keyint=24:scenecut=0:b-adapt=1:b-pyramid=0:tskip=1 -movflags +faststart -map_chapters -1 -f mp4 -vcodec libx265 -b:v 8000k -minrate 8000k -maxrate 8000k -bufsize 24000k -s:v:0 ${OUTPUT_WIDTH}x${OUTPUT_HEIGHT} -coder 1 -map 0:0 -map 1:1 -acodec libfaac -ac 2 -ab 128k -ar 44100 -pass 2 A_${2}
+
+ffmpeg -y -f rawvideo -s ${OUTPUT_WIDTH}x${OUTPUT_HEIGHT} -r ${FRAME_RATE} -i AP_${2}.yuv -i $1 -preset fast -x265-params level=5.1:bitrate=8000:vbv-bufsize=24000:bframes=3:ref=4:keyint=48:min-keyint=24:scenecut=0:b-adapt=1:b-pyramid=0:tskip=1 -movflags +faststart -map_chapters -1 -f mp4 -vcodec libx265 -b:v 8000k -minrate 8000k -maxrate 8000k -bufsize 24000k -s:v:0 ${OUTPUT_WIDTH}x${OUTPUT_HEIGHT} -coder 1 -map 0:0 -map 1:1 -acodec libfaac -ac 2 -ab 128k -ar 44100 -pass 2 AP_${2}
+
 
