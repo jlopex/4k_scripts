1e0664b867280ee217794413c3ab43f717216c77
Javier Lopez <javier.lopez@wuaki.tv>
2014-08-14 09:18:06 +0200

    refactor and add configurable logo margin

--- generate_4k.sh
+++ generate_4k.sh
@@ -15,40 +15,66 @@ MIN_RATE=${BITRATE}								#kbps
 MAX_RATE=${BITRATE}								#kbps
 FADE_DURATION=50									#frames
 FADE_MARGIN=100										#frames
+LOGO_WIDTH_MARGIN=30							#px
+LOGO_HEIGHT_MARGIN=30							#px
 
 # simple usage for user
-usage() { echo "Usage: $0 [-i <input_video>] [-o <output_video>] optional {[-l <logo_file>], [-w]}" 1>&2; exit 1; }
+usage() {
+    echo "Usage: $0 [-i <input_video>] [-o <output_video>] optional {[-l <logo_file>], [-w]}" 1>&2
+    exit 1
+}
+
+start_end_logo_frames() {
+
+    FRAMES=$(ffmpeg -i ${INPUT_VIDEO} -vcodec copy -f rawvideo -y /dev/null 2>&1 | tr ^M '\n' | awk '/^frame=/ {print $2}'| tail -n 1)
+    START_LOGO_FRAME=${FADE_MARGIN}
+    END_LOGO_FRAME=$((${FRAMES}-${FADE_MARGIN}))
+}
+
+yuv() {
+    ffmpeg -i ${INPUT_VIDEO} -f rawvideo -pix_fmt yuv420p -vf "scale=iw*sar:ih , pad=max(iw\,ih*(16/9)):ow/(16/9):(ow-iw)/2:(oh-ih)/2, scale=${OUTPUT_WIDTH}x${OUTPUT_HEIGHT}"  ${OUTPUT_VIDEO}.yuv
+}
+
+yuv_with_logo() {
+
+    start_end_logo_frames
+    ffmpeg -i ${INPUT_VIDEO} -loop 1 -i ${LOGO_FILE} -f rawvideo -pix_fmt yuv420p -filter_complex "[0:v] scale=iw*sar:ih , pad=max(iw\,ih*(16/9)):ow/(16/9):(ow-iw)/2:(oh-ih)/2, scale=${OUTPUT_WIDTH}x${OUTPUT_HEIGHT} [scaled]; [1:v] fade=in:${START_LOGO_FRAME}:${FADE_DURATION}:alpha=1, fade=out:${END_LOGO_FRAME}:${FADE_DURATION}:alpha=1 [logo]; [scaled][logo] overlay=W-w-${LOGO_WIDTH_MARGIN}:H-h-${LOGO_HEIGHT_MARGIN}:shortest=1"  ${OUTPUT_VIDEO}.yuv
+}
+
+yuv_with_logo_and_watermarking_A() {
+
+    ffmpeg -i ${INPUT_VIDEO} -loop 1 -i ${LOGO_FILE} -f rawvideo -pix_fmt yuv420p -filter_complex "[0:v] scale=iw*sar:ih , pad=max(iw\,ih*(16/9)):ow/(16/9):(ow-iw)/2:(oh-ih)/2, scale=${OUTPUT_WIDTH}x${OUTPUT_HEIGHT} [scaled]; [1:v] fade=in:${START_LOGO_FRAME}:${FADE_DURATION}:alpha=1, fade=out:${END_LOGO_FRAME}:${FADE_DURATION}:alpha=1 [logo]; [scaled][logo] overlay=W-w-${LOGO_WIDTH_MARGIN}:H-h-${LOGO_HEIGHT_MARGIN}:shortest=1" -f rawvideo - | NGS_FragmentPreprocessorYUV -w ${OUTPUT_WIDTH} -h ${OUTPUT_HEIGHT} -f ${FRAME_RATE} --A > ${OUTPUT_VIDEO}.yuvA
+}
+
+yuv_with_logo_and_watermarking_AP() {
+    ffmpeg -i ${INPUT_VIDEO} -loop 1 -i ${LOGO_FILE} -f rawvideo -pix_fmt yuv420p -filter_complex "[0:v] scale=iw*sar:ih , pad=max(iw\,ih*(16/9)):ow/(16/9):(ow-iw)/2:(oh-ih)/2, scale=${OUTPUT_WIDTH}x${OUTPUT_HEIGHT} [scaled]; [1:v] fade=in:${START_LOGO_FRAME}:${FADE_DURATION}:alpha=1, fade=out:${END_LOGO_FRAME}:${FADE_DURATION}:alpha=1 [logo]; [scaled][logo] overlay=W-w-${LOGO_WIDTH_MARGIN}:H-h-${LOGO_HEIGHT_MARGIN}:shortest=1" - | NGS_FragmentPreprocessorYUV -w ${OUTPUT_WIDTH} -h ${OUTPUT_HEIGHT} -f ${FRAME_RATE} --APrime > ${OUTPUT_VIDEO}.yuvAP
+}
+
+yuv_with_watermarking_A() {
+    ffmpeg -i ${INPUT_VIDEO} -f rawvideo -pix_fmt yuv420p -vf "scale=iw*sar:ih , pad=max(iw\,ih*(16/9)):ow/(16/9):(ow-iw)/2:(oh-ih)/2, scale=${OUTPUT_WIDTH}x${OUTPUT_HEIGHT}" - | NGS_FragmentPreprocessorYUV -w ${OUTPUT_WIDTH} -h ${OUTPUT_HEIGHT} -f ${FRAME_RATE} --A > ${OUTPUT_VIDEO}.yuvA
+}
+
+yuv_with_watermarking_AP() {
+    ffmpeg -i ${INPUT_VIDEO} -f rawvideo -pix_fmt yuv420p -vf "scale=iw*sar:ih , pad=max(iw\,ih*(16/9)):ow/(16/9):(ow-iw)/2:(oh-ih)/2, scale=${OUTPUT_WIDTH}x${OUTPUT_HEIGHT}" - | NGS_FragmentPreprocessorYUV -w ${OUTPUT_WIDTH} -h ${OUTPUT_HEIGHT} -f ${FRAME_RATE} --APrime > ${OUTPUT_VIDEO}.yuvAP
+}
 
 # create two yuvs files with resampling, logo and watermarking
 yuvs_with_logo_and_watermarking() {
 
-    FRAMES=$(ffmpeg -i ${INPUT_VIDEO} -vcodec copy -f rawvideo -y /dev/null 2>&1 | tr ^M '\n' | awk '/^frame=/ {print $2}'| tail -n 1)
-    START_LOGO_FRAME=${FADE_MARGIN}
-    END_LOGO_FRAME=$((${FRAMES}-${FADE_MARGIN}))
-
-    ffmpeg -i ${INPUT_VIDEO} -loop 1 -i ${LOGO_FILE} -f rawvideo -pix_fmt yuv420p -filter_complex "[0:v] scale=iw*sar:ih , pad=max(iw\,ih*(16/9)):ow/(16/9):(ow-iw)/2:(oh-ih)/2, scale=${OUTPUT_WIDTH}x${OUTPUT_HEIGHT} [scaled]; [1:v] fade=in:${START_LOGO_FRAME}:${FADE_DURATION}:alpha=1, fade=out:${END_LOGO_FRAME}:${FADE_DURATION}:alpha=1 [logo]; [scaled][logo] overlay=W-w-30:H-h-30:shortest=1" -f rawvideo - | NGS_FragmentPreprocessorYUV -w ${OUTPUT_WIDTH} -h ${OUTPUT_HEIGHT} -f ${FRAME_RATE} --A > ${OUTPUT_VIDEO}.yuvA
-
-    ffmpeg -i ${INPUT_VIDEO} -loop 1 -i ${LOGO_FILE} -f rawvideo -pix_fmt yuv420p -filter_complex "[0:v] scale=iw*sar:ih , pad=max(iw\,ih*(16/9)):ow/(16/9):(ow-iw)/2:(oh-ih)/2, scale=${OUTPUT_WIDTH}x${OUTPUT_HEIGHT} [scaled]; [1:v] fade=in:${START_LOGO_FRAME}:${FADE_DURATION}:alpha=1, fade=out:${END_LOGO_FRAME}:${FADE_DURATION}:alpha=1 [logo]; [scaled][logo] overlay=W-w-30:H-h-30:shortest=1" - | NGS_FragmentPreprocessorYUV -w ${OUTPUT_WIDTH} -h ${OUTPUT_HEIGHT} -f ${FRAME_RATE} --APrime > ${OUTPUT_VIDEO}.yuvAP
-
-}
-
-# create a single yuv with logo
-yuv_with_logo() {
-
-    FRAMES=$(ffmpeg -i ${INPUT_VIDEO} -vcodec copy -f rawvideo -y /dev/null 2>&1 | tr ^M '\n' | awk '/^frame=/ {print $2}'| tail -n 1)
-    START_LOGO_FRAME=${FADE_MARGIN}
-    END_LOGO_FRAME=$((${FRAMES}-${FADE_MARGIN}))
-
-    ffmpeg -i ${INPUT_VIDEO} -loop 1 -i ${LOGO_FILE} -f rawvideo -pix_fmt yuv420p -filter_complex "[0:v] scale=iw*sar:ih , pad=max(iw\,ih*(16/9)):ow/(16/9):(ow-iw)/2:(oh-ih)/2, scale=${OUTPUT_WIDTH}x${OUTPUT_HEIGHT} [scaled]; [1:v] fade=in:${START_LOGO_FRAME}:${FADE_DURATION}:alpha=1, fade=out:${END_LOGO_FRAME}:${FADE_DURATION}:alpha=1 [logo]; [scaled][logo] overlay=W-w-30:H-h-30:shortest=1"  ${OUTPUT_VIDEO}.yuv
+    # Calculate start end logo frame
+    start_end_logo_frames
+    # Generate A yuv
+    yuv_with_logo_and_watermarking_A
+    # Generate AP yuv
+    yuv_with_logo_and_watermarking_AP
 }
 
 yuvs_with_watermarking() {
-    ffmpeg -i ${INPUT_VIDEO} -f rawvideo -pix_fmt yuv420p -vf "scale=iw*sar:ih , pad=max(iw\,ih*(16/9)):ow/(16/9):(ow-iw)/2:(oh-ih)/2, scale=${OUTPUT_WIDTH}x${OUTPUT_HEIGHT}" - | NGS_FragmentPreprocessorYUV -w ${OUTPUT_WIDTH} -h ${OUTPUT_HEIGHT} -f ${FRAME_RATE} --APrime > ${OUTPUT_VIDEO}.yuv
-}
 
-# create a single yuv without logo
-yuv() {
-    ffmpeg -i ${INPUT_VIDEO} -f rawvideo -pix_fmt yuv420p -vf "scale=iw*sar:ih , pad=max(iw\,ih*(16/9)):ow/(16/9):(ow-iw)/2:(oh-ih)/2, scale=${OUTPUT_WIDTH}x${OUTPUT_HEIGHT}"  ${OUTPUT_VIDEO}.yuv
+    # Generate A yuv
+    yuv_with_watermarking_A
+    # Generate AP yuv
+    yuv_with_watermarking_AP
 }
 
 # create h265 output from yuv files
@@ -73,6 +99,9 @@ h265_two_pass_from_non_watermarked_yuv() {
     ffmpeg -y -f rawvideo -s ${OUTPUT_WIDTH}x${OUTPUT_HEIGHT} -r ${FRAME_RATE} -i ${OUTPUT_VIDEO}.yuv -i ${INPUT_VIDEO} -preset fast -x265-params level=5.1:bitrate=${BITRATE}:vbv-bufsize=${BUFFER_SIZE}:bframes=3:ref=4:keyint=48:min-keyint=24:scenecut=0:b-adapt=1:b-pyramid=0:tskip=1 -movflags +faststart -map_chapters -1 -f mp4 -vcodec libx265 -b:v ${BITRATE}k -minrate ${MIN_RATE}k -maxrate ${MAX_RATE}k -bufsize ${BUFFER_SIZE}k -s:v:0 ${OUTPUT_WIDTH}x${OUTPUT_HEIGHT} -coder 1 -map 0:0 -map 1:1 -acodec libfaac -ac 2 -ab 128k -ar 44100 -pass 2 ${OUTPUT_VIDEO}
 
 }
+
+#### MAIN ####
+set -e
 
 while getopts ":i:o:l:w" o; do
     case "${o}" in
